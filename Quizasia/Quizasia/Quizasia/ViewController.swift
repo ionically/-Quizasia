f4b5bfd55cbf118040f97517a6093abe12efff44
ionically <chandnichaudhari.66@gmail.com>
2022-12-01 16:57:57 +0530

    Initial commit

---
+++ Quizasia/Quizasia/Quizasia/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Quizasia
+//
+//  Created by chandni chaudhari on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: Data
+    
+    func makeArray() {
+        
+        var array = [Int]()
+        
+    }
+    
+}
+        
+    
+    
+
+
